a631c7e2e65009a891ec55486c4a9fb44dd1958b
Hoshimon <etsukohoshi1997@gmail.com>
2017-04-04 18:30:17 -0300

    Added User Auth

--- ejemplo/calendars/models.py
+++ ejemplo/calendars/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class MyCalendar(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, null=True, blank=True)
     class_name = models.CharField(max_length=150)
     class_url = models.URLField()
     class_date = models.DateField()
--- ejemplo/ejemplo/urls.py
+++ ejemplo/ejemplo/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
+from log.forms import LoginForm
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^calendar/', include('calendars.urls')),
+    url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
+    url(r'^logout/$', views.logout, {'next_page': '/login'}, name='logout'),
 ]
